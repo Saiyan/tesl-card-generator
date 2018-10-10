1dae65d812f67271fd209d4d40293a385420d462
Saiyan <saiyan@cssai.eu>
2018-10-10 17:45:23 +0200

    wrong event name for save button

--- script.js
+++ script.js
@@ -112,7 +112,7 @@ window.onload = function(){
             a.click();
         }, "image/png");
     };
-    btnSaveImage.touchend = btnSaveImage.onclick;
+    btnSaveImage.ontouchend = btnSaveImage.onclick;
 
     function drawCardImage() {
         let ctx = canvas.getContext("2d");
